e24aa3adb40c583db87f1a1066df50580490bee6
DATA GURU <gyrogodnon@gmail.com>
2024-12-01 16:14:22 +0530

    commit

--- MLOPS PROJECT - 6/PROJECT - 6/config/paths_config.py
+++ MLOPS PROJECT - 6/PROJECT - 6/config/paths_config.py
@@ -5,3 +5,9 @@ RAW_DATA_PATH = os.path.join(ARTIFACTS_DIR,"raw","data.csv")
 INGESTED_DATA_DIR = os.path.join(ARTIFACTS_DIR,"ingested_data")
 TRAIN_DATA_PATH = os.path.join(INGESTED_DATA_DIR , "train.csv")
 TEST_DATA_PATH = os.path.join(INGESTED_DATA_DIR , "test.csv")
+
+PROCESSED_DIR = os.path.join(ARTIFACTS_DIR,"processed_data")
+PROCESSED_DATA_PATH = os.path.join(ARTIFACTS_DIR,"processed_data","processed_train.csv")
+
+ENGINEERED_DIR = os.path.join(ARTIFACTS_DIR,"engineered_data")
+ENGINEERED_DATA_PATH = os.path.join(ARTIFACTS_DIR,"engineered_data","final_df.csv")
